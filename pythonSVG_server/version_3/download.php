d326f45a09ca79f208aa02524c592f51f8f39a0e
Leen Remmelzwaal <leen@siyavula.com>
2012-11-26 16:55:51 +0200

    folders removed during renaming & re-sorting

--- pythonSVG_server/version_3/download.php
+++
@@ -1,5 +0,0 @@
-<?php 
-   header("Content-Type: application/octet-stream");
-   header("Content-Disposition: attachment; filename=".$_GET['path']);
-   readfile($_GET['path']);
-?>
--- pythonSVG_server/version_4/download.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	 header("Title: ");
-   header("Content-Type: application/octet-stream");
-   header("Content-Disposition: attachment; filename='image.png'");
-   readfile($_GET['path']);
-?>
